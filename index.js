3426160fce3723c814adc6ec8fb90cc8808c3ccb
Ilya Kyazimov <40570012+BruceWayne3@users.noreply.github.com>
2020-10-25 14:10:58 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const data = {
+    little: {
+        weight: '1,8 - 2 кг',
+        portions: '6 - 8',
+        price: '3800 руб'
+    },
+    middle: {
+        weight: '2 - 2,5 кг',
+        portions: '8 - 10',
+        price: '4300 руб'
+    },
+    big: {
+        weight: '3 - 3,25 кг',
+        portions: '12 - 14',
+        price: '5300'
+    }
+}
+
+const buttons = document.querySelectorAll('.catalog__button')
